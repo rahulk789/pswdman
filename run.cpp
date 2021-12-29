adaa5c08e7a729ca1bd1fa5fb0c00f5f721adf1c
Rahul k <83643646+rahulk789@users.noreply.github.com>
2021-12-29 09:24:35 +0530

    added working passgen

--- run.cpp
+++ run.cpp
@@ -4,15 +4,13 @@ int main() {
     srand(time(0));
     string name;
     string pass;
-<<<<<<< HEAD
     if (token[0]== "add"){
         cout << "enter name of the website/app";
-=======
+
     /*
     // feature will be added later cout << "Enter the name and password to store"; 
     if (token[0]== "add"){*/
         cout << "enter name of the website/app \n";
->>>>>>> 2262269 (added basic password gen)
         cin >> name ;           
         passgen pg;
         pass=pg.passgenerator();
