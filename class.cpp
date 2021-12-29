f52d1465906383e8b1775ccd24f01b93508c418f
rahulk789 <rahul.u.india@gmail.com>
2021-12-29 09:13:19 +0530

    adding working passgen

--- class.cpp
+++ class.cpp
@@ -1,54 +1,55 @@
+#include <algorithm>
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
 #include <time.h>
-#include <string.h>
+#include <string>
+#include <cstdlib>
 using namespace std;
 class passgen {
-    
-    private:
-    //srand(time(0));
     int i=0;
-   
     public:
     string alpha();
     string num();
     string symb();
     string passgenerator(); 
 };
-passgen::string alpha(){
-        const string a ="abcdefghijklmnopqrstuvwxyz"; //total 26 alphabets
-        const string A ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        string pass1;
-        for (i=0;i<8;i++){
-            if (rand()%2==0)
-                pass1= pass1 + a[(rand()%27)];
-            else
-                pass1= pass1 + A[rand()%27];
-        }
-        return pass1;
+string passgen::alpha(){
+    const string a ="abcdefghijklmnopqrstuvwxyz"; //total 26 alphabets
+    const string A ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string pass1;
+    for (i=0;i<8;i++){
+        if (rand()%2==0)
+             pass1= pass1 + a[(rand()%27)];
+        else
+             pass1= pass1 + A[rand()%27];
     }
-passgen::string num(){
-        const string n ="0123456789";
-        string pass2;
-        for (i=0;i<4;i++){
-            pass2 = pass2 + n[rand()%10];
-        }
-        return pass2;
+    return pass1;
     }
-passgen::string symb(){
-        const string s ="!@#$%^&*()_+[]";
-        string pass3;
-        for (i=0;i<4;i++){
-            pass3 = pass3 + s[rand()%14];
-        }
-        return pass3;
+string passgen::num(){
+    const string n ="0123456789";
+    string pass2;
+    for (i=0;i<4;i++){
+        pass2 = pass2 + n[rand()%10];
+     }
+    return pass2;
     }
-passgen::string passgenerator(){
-        string pass1=alpha();
-        string pass2=num();
-        string pass3=symb();
+string passgen::symb(){
+    const string s ="!@#$%^&*()_+[]";
+    string pass3;
+    for (i=0;i<4;i++){
+        pass3 = pass3 + s[rand()%14];
     }
+    return pass3;
+    }
+string passgen::passgenerator(){
+    string pass;
+    string pass1=alpha();
+    string pass2=num();
+    string pass3=symb();
+    pass=pass1+pass2+pass3;
+    return pass;    
+}
 
 class passstore {
     ofstream file;
@@ -59,13 +60,13 @@ class passstore {
         this->pass = pass;
     }
     void writeit(){
-        file.open(name+".txt");
+        file.open(name,ios::in);
         file << pass << endl;
         file.close();
     }
 };
 class passret {
-    ofstream file;
+    ifstream file;
     string masterpass;
     string name;
     string text;
@@ -74,8 +75,12 @@ class passret {
         this->masterpass = masterpass;
     }
     void readit(){
-        file.open(name+".txt");
-        getline(file,text);
-        cout << text << endl;
+        file.open(name,ios::out);
+        while(!file.eof())
+        {
+            cout << text << endl;
+            file >> text;
+        }
+        file.close();
     }
 };
--- run.cpp
+++ run.cpp
@@ -1,21 +1,29 @@
 #include "class.cpp"
 
 int main() {
+    srand(time(0));
     string name;
     string pass;
+<<<<<<< HEAD
     if (token[0]== "add"){
         cout << "enter name of the website/app";
+=======
+    /*
+    // feature will be added later cout << "Enter the name and password to store"; 
+    if (token[0]== "add"){*/
+        cout << "enter name of the website/app \n";
+>>>>>>> 2262269 (added basic password gen)
         cin >> name ;           
         passgen pg;
-        pass=pg.passgen();
-        passstore ps(string name,string pass);
-        ps.writeit(string name);
-    }
+        pass=pg.passgenerator();
+        //passstore ps(string name,string pass);
+        //ps.writeit(string name);
+        cout << "the pass is " << pass << " \n";
+        /* }
     if (token[0]== "retrieve"){
         cout << "enter the master password";
         cin >> masterpass ;
         passret pr(string name, string masterpass);
-        pr.readit;
-    }
+        pr.readit;*/
     return 0;
   }
